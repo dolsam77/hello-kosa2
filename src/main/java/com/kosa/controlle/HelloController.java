9c34daafbfc238cadaada3d363c3e1465807d9d3
dolsam77 <dolsam77@nate.com>
2023-09-08 13:10:35 +0900

    first commit

---
+++ src/main/java/com/kosa/controlle/HelloController.java
@@ -0,0 +1,18 @@
+package com.kosa.controlle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("message", "Hello Kosa2!!!");
+		
+		return "hello";
+	}
+
+}
