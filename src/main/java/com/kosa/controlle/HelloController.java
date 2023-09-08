bfe466a46a8d358dc5997039d929ebdac09cd007
dolsam77 <dolsam77@nate.com>
2023-09-08 14:31:14 +0900

    PollSCM

--- src/main/java/com/kosa/controlle/HelloController.java
+++ src/main/java/com/kosa/controlle/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	
 	@GetMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("message", "Hello Kosa2!!!");
+		model.addAttribute("message", "Hello Kosa3333!!!");
 		
 		return "hello";
 	}
